71646f824b686d367052d0312dd62f0b9fb49a8d
Hasanur Rahman Sazib <odsazib@gmail.com>
2017-01-08 22:21:57 +0600

    Update variant.sh
    Added missing bracket in comment

--- variant.sh
+++ variant.sh
@@ -7,6 +7,6 @@ mkdir -p /data/variant
 cd /data/variant
 cat /proc/meminfo > meminfo.txt
 awk 'NR==1{print substr($2,1,1)}' meminfo.txt > ram.txt
-awk '{print "ram=" $0}' ram.txt | tee -a /data/*/mods.prop ## In my case, mods.prop is a get_prop file (in data/$/ dir to be used in assert_check
+awk '{print "ram=" $0}' ram.txt | tee -a /data/*/mods.prop ## In my case, mods.prop is a get_prop file (in data/$/ dir) to be used in assert_check
 rm -rf /data/variant
 exit 0
